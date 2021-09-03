d48471ad0d17107d11339726147455620409a369
Benjamin Tran <Benjietran1994@gmail.com>
2021-09-03 16:53:28 -0700

    correction

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -6,23 +6,10 @@ import scrape_mars
 
 app = Flask(__name__)
 
-conn = "mongodb://localhost:27017/mars_app"
-client = pymongo.MongoClient(conn)
-db=client.mars_db
-
-
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
 
 @app.route("/")
-def home():
-    Mars_dict = db.Mars_dict.find_one()
-    # Return template and data
-    return render_template("index.html", mars=Mars_dict)
-@app.route("/scrape")
-def scrape():
-    Mars_dict = db.Mars_dict
-    mars_data = scrape_mars.scrape()
-    dict.update({}, mars_data, upsert=True)
-    return redirect("/")
-
-if __name__ == "__main__":
-    app.run(debug=True)
+def index():
+    Mars_dict = mongo.db.Mars_dict.find_one()
+    return render_template("index.html",dict=Mars_dict)
