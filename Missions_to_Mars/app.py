8750410ea45f4ea610961f88dc6a93d7ba5a5b00
Benjamin Tran <Benjietran1994@gmail.com>
2021-09-03 14:41:30 -0700

    scrape mars

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,22 +1,24 @@
 # Dependencies
-from flask import Flask, render_template, redirect
-from flask_pymongo import PyMongo
+from flask import Flask, render_template
+import pymongo
 import scrape_mars
 
 app = Flask(__name__)
 
-mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
+conn = "mongodb://localhost:27017"
+client = pymongo.MongoClient(conn)
+db=client.mars_db
 
 
 
 @app.route("/")
 def home():
-    Mars_dict = mongo.db.Mars_dict.find_one()
+    Mars_dict = db.Mars_dict.find_one()
     # Return template and data
     return render_template("index.html", mars=Mars_dict)
 @app.route("/scrape")
 def scrape():
-    Mars_dict = mongo.db.Mars_dict
+    Mars_dict = db.Mars_dict
     mars_data = scrape_mars.scrape()
     dict.update({}, mars_data, upsert=True)
     return redirect("/")
