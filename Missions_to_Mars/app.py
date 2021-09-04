a6b17de6f2e276d9f340ed17da9a7e428b891106
Benjamin Tran <Benjietran1994@gmail.com>
2021-09-03 17:03:10 -0700

    Mars App

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -13,3 +13,17 @@ mongo = PyMongo(app)
 def index():
     Mars_dict = mongo.db.Mars_dict.find_one()
     return render_template("index.html",dict=Mars_dict)
+@app.route("/scrape")
+def scrape():
+
+    # Run the scrape function
+    mars_data = scrape_mars.scrape_info()
+
+    # Update the Mongo database using update and upsert=True
+    mongo.db.collection.update({}, mars_data, upsert=True)
+
+    # Redirect back to home page
+    return redirect("/")
+
+if __name__ == "__main__":
+    app.run(debug=True)
